834e6dba41b09a465ed3e507b9d04b139aee8ce9
modyelgen <modyelgen243@gmail.com>
2025-01-01 20:29:05 +0200

    edit some code and read me

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Minimum Value Finder
 
 ## Overview
-This Dart program allows users to input a list of numbers and find the minimum value from the list. The program continuously accepts numbers from the user until the user types 'q' or 'Q'. If valid numbers are entered, it calculates and displays the smallest value in the list. If no valid input is provided, it will notify the user.
+This dart program allows users to input a list of numbers and find the minimum value from the list. The program still accepts numbers from the user until the user types 'q' or 'Q'. If valid numbers are entered, it calculates and displays the smallest value in the list. If the user input is not matching with number form
 
 ## Code Explanation
-Collecting Numbers: The program uses a(Do -While) loop to continuously prompt the user for numbers. It checks if the input is numeric or a quit command ('q').
+Collecting Numbers: The program uses a(Do -While) loop to always aske the user to enter numbers. It checks if the input is number or a quit command ('q').
 
-Finding the Minimum: Once the user decides to quit, the program calculates the minimum value using reduce() for efficient comparison across the list of numbers.
+Finding the Minimum: Once the user enter q or Q to quit, the program calculates the minimum value by firs sorting the number whih sort the number from lowest to biggest , and then we access the first item to print it .
--- lib/min_value.dart
+++ lib/min_value.dart
@@ -3,43 +3,42 @@ import 'dart:io';
 void getMinValue() {
   String? input;
   List<int> numbers = [];
-  
+
   // welcome message
   print("Hello there! Let's find the minimum value in a list of numbers.\n"
       "Please enter a number and press ENTER. To end the input and see the result, type 'q' or 'Q'.");
 
-  // Loop to collect inputs until the user enter q to exist
+  // make do while loop to take all user input and add all in the list
   do {
     print("Enter a number (or 'q' to finish):");
-    input = stdin.readLineSync();
+    input = stdin.readLineSync(); //take input from user
 
-    // Check if input is valid or no space within it
+    // validate if the input not null or not space
     if (input != null && input.trim().isNotEmpty) {
-      // Try to parse the input as a double
-      
-      // return null incase failed parsing
+      // make try pare to convert string into number
       int? number = int.tryParse(input);
 
       // If number is valid then we add to list
       if (number != null) {
         numbers.add(number);
       }
-      // If input is 'q' or 'Q', exit loop
+      // If input is 'q' or 'Q', end loop
       else if (input.toLowerCase() == 'q') {
         print("Exiting input mode. Calculating the result...");
       }
-      // Invalid number, and repeat entering new number again
+      // show error incase invalid input
       else {
         print("Invalid input. Please enter a valid number or 'q' to quit.");
       }
     }
+    //as long as user not null or input is not equal q
   } while (input != null && input.toLowerCase() != 'q');
 
   // If list is not empty, find the minimum value
   if (numbers.isNotEmpty) {
-    // Finding the minimum value
-    int minValue = numbers.reduce((a, b) => a < b ? a : b);
-    print("The minimum value in the list is: $minValue");
+    // order all the list from lowest to biggest using sort function.
+    numbers.sort();
+    print("The minimum value in the list is: ${numbers.first}");
   } else {
     print("No valid data entered.");
   }
